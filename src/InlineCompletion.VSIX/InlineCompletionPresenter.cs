4c9fd8eb97edb8affac287cbbc1d6e65d425de24
Amadeusz Wieczorek <amwieczo@microsoft.com>
2020-07-26 20:41:34 -0700

    initial setup, add mef parts

---
+++ src/InlineCompletion.VSIX/InlineCompletionPresenter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion;
+using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion.Data;
+using System;
+
+namespace InlineCompletion.VSIX
+{
+    internal class InlineCompletionPresenter : ICompletionPresenter
+    {
+        private InlineCompletionPresenterProvider factory;
+
+        public InlineCompletionPresenter(InlineCompletionPresenterProvider factory)
+        {
+            this.factory = factory;
+        }
+
+        public event EventHandler<CompletionFilterChangedEventArgs> FiltersChanged;
+        public event EventHandler<CompletionItemSelectedEventArgs> CompletionItemSelected;
+        public event EventHandler<CompletionItemEventArgs> CommitRequested;
+        public event EventHandler<CompletionClosedEventArgs> CompletionClosed;
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Open(IAsyncCompletionSession session, CompletionPresentationViewModel presentation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(IAsyncCompletionSession session, CompletionPresentationViewModel presentation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/InlineCompletion.VSIX/InlineCompletionPresenterProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion;
+using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion.Data;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InlineCompletion.VSIX
+{
+    [Export(typeof(ICompletionPresenterProvider))]
+    [ContentType("text")]
+    [Order(Before = PredefinedCompletionNames.DefaultCompletionPresenter)]
+    internal class InlineCompletionPresenterProvider : ICompletionPresenterProvider
+    {
+        public CompletionPresenterOptions Options { get; } = new CompletionPresenterOptions(resultsPerPage: 1);
+
+        public ICompletionPresenter GetOrCreate(ITextView textView)
+        {
+            return new InlineCompletionPresenter(this);
+        }
+    }
+}
